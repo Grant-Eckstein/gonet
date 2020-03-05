8808241595a06a8af2b564d1f5b526c6b76fc75f
Grant Eckstein <grant0eckstein@gmail.com>
2020-03-04 19:50:06 -0600

    Finished project

--- gonet.go
+++ gonet.go
@@ -1,28 +1,89 @@
-package main
+package gonet
 
 import (
-	"fmt"
+	"bytes"
+	"encoding/json"
 	"log"
+	"net"
 	"os"
 )
 
-type Listener struct {
-	Name   string
-	CmdMap map[string]func(data []byte) []byte
+func check(e error) {
+	if e != nil {
+		//log.Fatal(e)
+		panic(e)
+	}
 }
 
-func newListener(Name string) Listener {
-	listener := Listener{Name: Name}
-	listener.CmdMap = make(map[string]func(data []byte) []byte)
-	return listener
+type Connection struct {
+	Protocol string
+	Length   int
+	Triggers map[string]func(data []byte) []byte
 }
 
-func (l *Listener) trigger(cmd string, function func(data []byte) []byte) {
-	l.CmdMap[cmd] = function
+type Message struct {
+	Cmd  string
+	Data []byte
 }
 
-func (l *Listener) lookup(cmd string) func(data []byte) []byte {
-	f := l.CmdMap[cmd]
+func NewMessage(cmd string, data []byte) Message {
+	return Message{Cmd: cmd, Data: data}
+}
+
+func (m *Message) Export() ([]byte, error) {
+	return json.Marshal(&m)
+}
+
+func NewConnection(protocol string, length int) Connection {
+	conn := Connection{Protocol: protocol,
+		Length:   length,
+		Triggers: make(map[string]func(data []byte) []byte),
+	}
+
+	return conn
+}
+
+func decodeMessage(data []byte) Message {
+
+	var m Message
+	err := json.Unmarshal(bytes.Trim(data, "\x00"), &m)
+	check(err)
+	return m
+}
+
+func (c *Connection) Trigger(cmd string, function func(data []byte) []byte) {
+	c.Triggers[cmd] = function
+}
+
+func (c *Connection) Listen(port string) Message {
+	ln, err := net.Listen(c.Protocol, port)
+	check(err)
+
+	conn, err := ln.Accept()
+	data := make([]byte, c.Length)
+	_, err = conn.Read(data)
+	conn.Close()
+	check(err)
+
+	m := decodeMessage(data)
+
+	return m
+}
+
+func (c *Connection) Send(address string, cmd string, data []byte) {
+	m := NewMessage(cmd, data)
+
+	j, _ := m.Export()
+
+	conn, err := net.Dial(c.Protocol, address)
+	check(err)
+
+	_, err = conn.Write(j)
+	conn.Close()
+}
+
+func (c *Connection) lookupTriggers(cmd string) func(data []byte) []byte {
+	f := c.Triggers[cmd]
 	if f != nil {
 		return f
 	} else {
@@ -30,23 +91,7 @@ func (l *Listener) lookup(cmd string) func(data []byte) []byte {
 		os.Exit(1)
 		return nil
 	}
-
 }
-
-func (l *Listener) check(cmd string, data []byte) []byte {
-	return l.lookup(cmd)(data)
+func (c *Connection) ResolveMessageTriggers(m Message) []byte {
+	return c.lookupTriggers(m.Cmd)(m.Data)
 }
-
-func test(data []byte) []byte {
-	return []byte(string(data)[:1])
-}
-
-func main() {
-	l := newListener("test")
-	l.trigger("hello", test)
-
-	output := l.check("hello", []byte("hello, world!"))
-	fmt.Println(string(output))
-
-}
-
